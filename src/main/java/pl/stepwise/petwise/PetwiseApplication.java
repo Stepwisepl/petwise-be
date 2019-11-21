2140a40902a77aee212ab061e3c69923531cab7a
Marceli Baczewski <marceli.baczewski@stepwise.pl>
2019-11-21 17:06:29 +0100

    Initial commit from Spring Initializr

---
+++ src/main/java/pl/stepwise/petwise/PetwiseApplication.java
@@ -0,0 +1,13 @@
+package pl.stepwise.petwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/stepwise/petwise/PetwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.stepwise.petwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
